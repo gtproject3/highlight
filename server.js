d9a0803e459dd995424f78d7da992f449a5471da
kcuellar <katherine.cuellar93@gmail.com>
2018-10-16 21:03:53 -0400

    added passport js files and functionality to server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//for Passport
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./backend/config/keys");
+const routes = require("./backend/routes");
+require("./backend/services/passport.js")(passport, db.User);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +21,24 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // app.use(routes);
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+//to use cookies for session
+app.use(
+  cookieSession({
+    //keep cookie session for 15 days
+    maxAge: 15*24*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+//tell passport to use cookies to manage authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect(
