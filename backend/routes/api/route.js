d2acbdbc73d3f097f47512a90755fd99a0d697e5
kcuellar <katherine.cuellar93@gmail.com>
2018-10-25 20:01:02 -0400

    got rid of stuff JWT wasn't using

--- backend/routes/api/route.js
+++ backend/routes/api/route.js
@@ -2,31 +2,31 @@
 const router = require("express").Router();
 const controller = require("../../controllers/controller");
 
-// how do we want to name them?
+// // how do we want to name them?
 
-// post route for inserting into our movie database
-router.route("/movie/:title/:imdbID")
-    .post(controller.insertMovie);
+// // post route for inserting into our movie database
+// router.route("/movie/:title/:imdbID")
+//     .post(controller.insertMovie);
 
-// put route for updating the blurb on the movie table
+// // put route for updating the blurb on the movie table
 
-router.route("/movie/:BlurbId")
-    .put(controller.updateMovieBlurb);
+// router.route("/movie/:BlurbId")
+//     .put(controller.updateMovieBlurb);
 
-// get route for seraching for a movie in our database by title
-router.route("/movie/:title")
-    .get(controller.findMovie);
+// // get route for seraching for a movie in our database by title
+// router.route("/movie/:title")
+//     .get(controller.findMovie);
 
-// post route for creating a blurb in our database
-router.route("/blurb")
-    .post(controller.insertBlurb);
+// // post route for creating a blurb in our database
+// router.route("/blurb")
+//     .post(controller.insertBlurb);
 
-// updating a blurb in our database
-router.route("/blurb")
-    .put(controller.updateBlurb);
+// // updating a blurb in our database
+// router.route("/blurb")
+//     .put(controller.updateBlurb);
 
-// find all blurbs with attached to a movie
-router.route("/blurb/:imdbID")
-    .get(controller.findBlurb);
+// // find all blurbs with attached to a movie
+// router.route("/blurb/:imdbID")
+//     .get(controller.findBlurb);
 
-module.exports = router;
+// module.exports = router;
