973247954bc6821e467d4ca6dfe66b59af2bcd8c
1661sons <julia.kerouac@gmail.com>
2018-10-27 12:30:18 -0400

    finished login page

--- client/src/components/pages/LoginPage/LoginPage.js
+++ client/src/components/pages/LoginPage/LoginPage.js
@@ -9,7 +9,7 @@ const LoginPage = props => {
 		<GlobalNav />
 		<h1 className="uk-text-center blueText">login</h1>
 		<div className="uk-container">
-		<form className="authorForm uk-width-1-2@m uk-align-center uk-form-stacked">
+			<form className="authorForm uk-width-1-2@m uk-align-center uk-form-stacked">
                 
 				<p className="uk-text-lead">Login and add some Highlights of your own.</p>
 				<p className="uk-text-center redText"><span uk-icon="bolt"></span></p>
@@ -21,7 +21,7 @@ const LoginPage = props => {
 							<label className="uk-form-label" for="form-stacked-select">* Email:</label>
 							<div className="uk-inline uk-width-1-1">
 								<span className="uk-form-icon uk-form-icon-flip blueText" uk-icon="icon: world"></span>
-								<input className="uk-input" id="email" name="email" type="text">
+								<input className="uk-input" id="email" name="email" type="text"></input>
 							</div>
 						</div>
 	
@@ -29,15 +29,19 @@ const LoginPage = props => {
 							<label className="uk-form-label" for="form-stacked-select">* Password:</label>
 							<div className="uk-inline uk-width-1-1">
 								<span className="uk-form-icon uk-form-icon-flip blueText" uk-icon="icon: lock"></span>
-								<input className="uk-input" id="password" name="password" type="text">
+								<input className="uk-input" id="password" name="password" type="text"></input>
 							</div>
 						</div>           
 					
-				<button className="uk-button uk-button-secondary redText uk-align-center" id="submit-btn"><span uk-icon="user"></span>&nbsp; login</button>
+					<button className="uk-button uk-button-secondary redText uk-align-center" id="submit-btn"><span uk-icon="user"></span>&nbsp; login</button>
+
+					</div>
 	
-			</fieldset>
-		</form>
+				</fieldset>
+			
+			</form>
 		</div>
+		<GlobalFooter />
 	</div>
 
 };
