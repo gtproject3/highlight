696afbd7ce0bae8a51340813f75c2e705f8d8d36
kcuellar <katherine.cuellar93@gmail.com>
2018-10-27 12:08:14 -0400

    working on converting components

--- client/src/components/AuthorCard/AuthorCard.js
+++ client/src/components/AuthorCard/AuthorCard.js
@@ -7,13 +7,13 @@ const AuthorCard = props => (
         <div className="uk-card uk-card-default">
             <div className="uk-card-header">
                 <div className="uk-width-expand">
-                    <div className="uk-card-badge uk-label">8 works</div>
-                    <h3 className="uk-card-title uk-margin-remove-bottom">James Baldwin</h3>
-                    <p className="uk-text-meta uk-margin-remove-top"><span className="flag-icon flag-icon-us"></span> New York City, New York, United States<br></br>Born: August 02, 1924<br></br>Died: December 01, 1987</p>
+                    <div className="uk-card-badge uk-label" id="worksCount">{props.worksCount}</div>
+                    <h3 className="uk-card-title uk-margin-remove-bottom" id="authorName">{props.authorName}</h3>
+                    <p className="uk-text-meta uk-margin-remove-top"><span className="flag-icon flag-icon-us"></span> New York City, New York, United States<br></br>Born: {props.bornAt}<br></br>Died: {props.diedAt}</p>
                 </div>
             </div>
             <div className="uk-card-body">
-                <p>James Arthur "Jimmy" Baldwin was an American novelist and social critic. His essays, as collected in Notes of a Native Son, explore intricacies of racial, sexual, and class distinctions in Western societies, most notably in mid-20th-century America.</p>
+                <p id="about">{props.about}</p>
             </div>
             <div className="uk-card-footer">
                 <a className="uk-align-right uk-button uk-button-text" href="baldwin.html">View Author<span uk-icon="arrow-right"></span></a>
