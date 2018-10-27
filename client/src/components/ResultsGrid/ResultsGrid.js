9cd4631a31a488ed77bb3eee8d3f978f707846a4
1661sons <julia.kerouac@gmail.com>
2018-10-27 13:09:22 -0400

    finished search results page

--- client/src/components/ResultsGrid/ResultsGrid.js
+++ client/src/components/ResultsGrid/ResultsGrid.js
@@ -4,22 +4,17 @@ import AuthorCard from '../AuthorCard';
 
 const ResultsGrid = props => (
 
-	<div class="uk-container searchResults">
-
-		<h1 class="whitishText searchContainerTitle uk-text-center resultsCount">5 results</h1>
-		<h6 class="uk-width-expand lightGreyText" uk-leader="fill: ∆"> </h6>
-		
-		<div class="uk-child-width-expand@m uk-grid-match" uk-grid="true">
+	<div>
+		<div class="uk-child-width-expand@s uk-grid-match" uk-grid="true">
 			<AuthorCard />
 			<AuthorCard />			
 		</div>
-		<div class="uk-child-width-expand@m uk-grid-match" uk-grid="true">
+		<div class="uk-child-width-expand@s uk-grid-match" uk-grid="true">
 			<AuthorCard />
 			<AuthorCard />	
 		</div>
-
 	</div>
-	
+
 );
 
 export default ResultsGrid;
--- client/src/components/pages/SearchResults/SearchResults.js
+++ client/src/components/pages/SearchResults/SearchResults.js
@@ -1,18 +1,24 @@
 import react from 'React';
+import './SearchResults.css';
 import GlobalNav from '../../GlobalNav';
 import GlobalFooter from '../../GlobalFooter';
-import './SearchResults.css';
+import ResultsGrid from '../../ResultsGrid';
+
+// there's probably a better way to do this- with a for loop
+// each ResultsGrid contsains 4 AuthorCards
 
 const SearchResults = props => (
 
 	<div>
-
 		<GlobalNav />
+		<div class="uk-container searchResults">
 
-
+			<h1 class="whitishText searchContainerTitle uk-text-center resultsCount">5 results</h1>
+			<h6 class="uk-width-expand lightGreyText" uk-leader="fill: ∆"> </h6>
+			<ResultsGrid />
+			
+		</div>
 		<GlobalFooter />
-
-
 	</div>
 
 );
