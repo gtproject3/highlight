432a92e46b41d2ed6901a45b04e64c9391603763
Sarah Subero <ssubero06@gmail.com>
2018-10-29 02:00:42 -0400

    RoutingAndBug

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-
 // should be importing pages here, not components
 import GlobalNav from './components/GlobalNav';
 import Hero from './components/Hero';
@@ -9,8 +8,8 @@ import SearchPanel from './components/SearchPanel';
 import AuthorForm from './pages/AuthorForm/AuthorForm';
 import HomeNav from './components/HomeNav/HomeNav';
 import Home from './pages/Home/Home';
-
-console.log("this is a pain");
+import CreateAccountPage from './pages/CreateAccountPage/CreateAccountPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 class App extends Component {
 
@@ -21,9 +20,9 @@ class App extends Component {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/AuthorForm" component={AuthorForm} />
+                    <Route path="/CreateAccountPage" component={CreateAccountPage} />
                     {/*<Route path="/details" component={AuthorsDetail}/>*/}
-                    <Route path="login" component={HomeNav} />
-                    <Route exact path="/" component={Home} />
+                    <Route path="LoginPage" component={LoginPage} />
 
                 </Switch>
             </Router>
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch } from 'react-router-dom';
 import App from './App';
-import HomeNav from './components/HomeNav/HomeNav';
 import GlobalNav from './components/GlobalNav';
 import AuthorForm from './pages/AuthorForm/AuthorForm';
 /*import AuthorsDetail from './pages/AuthorsDetail';*/
